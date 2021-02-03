ff20960ffec6b3026d1d357737db4192702de64c
Srinivas V <srinistocks2020@gmail.com>
2021-02-02 22:35:56 -0600

    simple-node-travis commit

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const favoriteFood = process.env.FAVORITE_FOOD;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+   
+	console.log(`My favorite food is ${favoriteFood}`);
+    await sleep(5000);
+  }
+}
+
+main();
